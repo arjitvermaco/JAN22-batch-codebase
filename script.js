a363a8e3dcb7e60238e2787178098eb00bbfd16a
Arjit <arjitverma.go@gmail.com>
2024-02-26 20:19:53 +0530

    Promise done with api calls

--- script.js
+++ script.js
@@ -10,37 +10,37 @@
 
 //Our First API call
 const countriesContainer = document.querySelector('.countries');
+const loader = document.querySelector('.loader');
+// function getCountryData(country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/alpha/${country}`);
+//   request.send();
 
-function getCountryData(country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/alpha/${country}`);
-  request.send();
+//   request.addEventListener('load', function () {
+//     // console.log(this.responseText)
+//     // console.log(JSON.parse(this.responseText))
+//     const data = JSON.parse(this.responseText);
+//     console.log(data[0]);
+//     renderData(data[0]);
 
-  request.addEventListener('load', function () {
-    // console.log(this.responseText)
-    // console.log(JSON.parse(this.responseText))
-    const data = JSON.parse(this.responseText);
-    console.log(data[0]);
-    renderData(data[0]);
+//     //Get neighbours data
+//     const neighbour = data[0].borders[0];
 
-    //Get neighbours data
-    const neighbour = data[0].borders[0];
+//     const request2 = new XMLHttpRequest();
+//     request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
 
-    const request2 = new XMLHttpRequest();
-    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
-    request2.send();
+//     request2.addEventListener('load', function () {
+//       const data2 = JSON.parse(this.responseText);
+//       console.log('Data2 is', data2);
+//       renderData(data2[0]);
 
-    request2.addEventListener('load', function () {
-      const data2 = JSON.parse(this.responseText);
-      console.log('Data2 is', data2);
-      renderData(data2[0]);
+//       //get more country
+//     });
+//   });
+// }
 
-      //get more country
-    });
-  });
-}
-
-getCountryData('in');
+// getCountryData('in');
 
 function renderData(data) {
   const html = `
@@ -57,19 +57,151 @@ function renderData(data) {
     `;
 
   countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
 }
-setTimeout(() => {
-    console.log('1 second passed');
-    setTimeout(() => {
-      console.log('2 seconds passed');
-      setTimeout(() => {
-        console.log('3 second passed');
-        setTimeout(() => {
-          console.log('4 second passed');
-        }, 1000);
-      }, 1000);
-    }, 1000);
-  }, 1000);
+// // setTimeout(() => {
+// //     console.log('1 second passed');
+// //     setTimeout(() => {
+// //       console.log('2 seconds passed');
+// //       setTimeout(() => {
+// //         console.log('3 second passed');
+// //         setTimeout(() => {
+// //           console.log('4 second passed');
+// //         }, 1000);
+// //       }, 1000);
+// //     }, 1000);
+// //   }, 1000);
 
-  //Promises
+//   //Promises
+
+//   const myPromise = new Promise((resolve,reject)=>{
+
+//       let randomNum = Math.random() + 1;
+
+//       setTimeout(()=>{
+//         if(randomNum > 0.5){
+//           resolve("Promise Was SuccessFull")
+//         }else{
+//           reject("Promise Failed!!!")
+//         }
+
+//       },2000)
+//   })
+
+//   myPromise.then((data)=>{console.log(data)}).catch(err => {console.log(err)})
+
+// Load a user's profile from a server.
+// Once the profile is loaded, fetch the user's posts.
+// After the posts are fetched, load comments for the first post.
+
+//Fetch
+
+// fetch('https://restcountries.com/v3.1/alpha/in').then((res)=>{
+//   return res.json()
+// }).then(data => {console.log(data)})
+
+// fetch('https://v2.jokeapi.dev/joke/Programming').then((response)=>{
+//   console.log(response)
+//   if(!response.ok){
+//     throw new Error("Network Response was not OK!!")
+//   }
+//    return response.json()
+// }).then(data =>{
+//     console.log("Data recieved from server", data)
+// }).catch(error => console.error("Failed to fetch data",error))
+
+const getCountryData = country => {
+  //Country 1
+  fetch(`https://restcountries.com/v3.1/alpha/${country}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Country not found ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      renderData(data[0]);
+      const neighbour = data[0].borders[0];
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Country not found ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => renderData(data[0]))
+    .catch(err => console.log(err))
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+      loader.style.opacity = 0;
+    });
+};
+
+// getCountryData('in');
+
+let api_key = '628324115113111858438x74715';
+
+function getUserLocation() {
+  //  navigator.geolocation.getCurrentPosition((position)=>{
+  //     console.log(position)
+  //  },(err)=>{
+  //   console.error(err)
+  //  })
+
+  // return new Promise(function (resolve, reject) {
+  //   navigator.geolocation.getCurrentPosition(
+  //     postion => resolve(postion),
+  //     err => reject(err)
+  //   );
+  // });
+
+  const locationPromise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      postion => {
+        resolve(postion);
+      },
+      err => {
+        reject(err);
+      }
+    );
+  });
+
+  return locationPromise;
+}
+
+const whereAmI = function () {
+  getUserLocation()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      console.log('Lat', lat);
+      console.log('Lang', lng);
+      return fetch(`https://geocode.xyz/${lat},${lng}?json=1&auth=${api_key}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error('Problem with geolocation api!!');
+      return res.json();
+    })
+    .then(data=>{
+      // console.log(data)
+      const userCountry = data.country.slice(0,2).toLowerCase();
+      return fetch(`https://restcountries.com/v3.1/alpha/${userCountry}`)
+
+
+      //Make country api call
+    })
+    .then(res => {
+      if (!res.ok) throw new Error('Problem with Country api!!');
+      return res.json();
+    })
+    .then(data=>{
+      console.log(data[0]);
+      renderData(data[0])
+    })
+    .catch(err => console.error("error with geolocation",err))
+    .finally(()=>{
+      countriesContainer.style.opacity = 1;
+      loader.style.opacity = 0
+    });
+  }
+whereAmI()
